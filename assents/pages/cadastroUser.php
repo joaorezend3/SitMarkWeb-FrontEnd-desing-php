d89ba04e7c3e9f1c52e6afe7bcb549d70c1003d6
Rezende <joaovitorezende45@gmail.com>
2023-10-14 16:38:57 -0300

    iniciando cadastro

--- assents/pages/cadastroUser.php
+++ assents/pages/cadastroUser.php
@@ -6,13 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cursos ALR</title>
     <link rel="stylesheet" href="../css/cfg.css">
-    <!--<link rel="stylesheet" href="../css/navbar.css">-->
+    <link rel="stylesheet" href="../css/navbar.css">
     <link rel="stylesheet" href="../css/cadastroUser.css">
 </head>
 
 <body>
+    <header>
+        <nav>
+          <div class="logo-holder logo-8">
+            <a href="../../index.html">
+              <h3>ALR <span>ALR</span></h3>
+            </a>
+          </div>
+          <ul>
+            <a href="marketing.html">
+              <li>Marketing</li>
+            </a>
+            <a href="cursos.index.html">
+              <li>Cursos</li>
+            </a>
+            <a href="#">
+              <li>Sobre Nós</li>
+            </a>
+            <a href="cadastroUser.php" class="btn-navBar"> Cadastre-se</a>
+            <a href="@" class="btn-navBar"> Conectar</a>
+          </ul>
+        </nav>
+      </header>
     <section>
-        <div class="card    ">
+        <div class="card">
             <form action="" method="POST">
                 <div>
                     <input type="text" id="nome" name="nome" required placeholder="Nome">
@@ -23,10 +45,12 @@
                     <input type="text" id="cep" name="cep" required placeholder="cep">
                 </div>
                 <input type="password" id="senha" name="senha" required placeholder="Senha: ****"><br>
+                <div>
                 <input type="submit" value="Cadastrar" class="btnCadastrar">
                 <p>
                     Ao continuar, você aceita o <a href="#">Termo de adesão</a> e <a href="#">Política de Privacidade</a>.
                 </p>
+                </div>  
             </form>
         </div>
 
